f1acd3a847c7255d14197f66e2aa7d31c1bbc366
Eric Jang <ericjang96@gmail.com>
2018-01-12 14:30:44 -0800

    Should try every combination of options for our case now. Still need to add the data exporting

--- selenium_scraper/scraper.py
+++ selenium_scraper/scraper.py
@@ -4,7 +4,6 @@ driver = webdriver.Chrome()
 driver.get("http://agcensus.dacnet.nic.in/DistCharacteristic.aspx")
 driver.set_page_load_timeout(60)
 
-# Get the 8 dropdowns from this page and put their options in their respective list
 dropdown_year = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlYear")
 num_options_year = len(dropdown_year.find_elements_by_tag_name("option"))
 
@@ -12,8 +11,10 @@ num_options_year = len(dropdown_year.find_elements_by_tag_name("option"))
 for index_year in range(0, num_options_year):
     dropdown_social_group = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlSocialGroup")
     # Count only the options where the value matches "ALL SOCIAL GROUPS"
+    for y in dropdown_social_group.find_elements_by_tag_name("option"):
+        print(y.get_attribute('value'))
     num_options_social_group = len([x for x in dropdown_social_group.find_elements_by_tag_name("option")
-                                    if x.get_attribute('value') == 'ALL SOCIAL GROUPS'])
+                                    if x.get_attribute('value') == '4'])
 
     for index_social_group in range(0, num_options_social_group):
         dropdown_state = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlState")
@@ -25,22 +26,24 @@ for index_year in range(0, num_options_year):
 
             for index_district in range(0, num_options_district):
                 dropdown_tables = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlTables")
-                num_options_tables = len(dropdown_tables.find_elements_by_tag_name("option"))
+                num_options_tables = len([x for x in dropdown_tables.find_elements_by_tag_name("option")
+                                    if x.get_attribute('value') == '6b'])
 
                 for index_tables in range(0, num_options_tables):
+                    [x for x in dropdown_tables.find_elements_by_tag_name("option")
+                     if x.get_attribute('value') == '6b'][0].click()
                     dropdown_crops = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlCrop")
-                    num_options_crops = len([x for x in dropdown_tables.find_elements_by_tag_name("option")
-                                    if x.get_attribute('value') == 'TENANCY' or x.get_attribute('value') == 'CROPPING PATTERN'])
+                    num_options_crops = len(dropdown_crops.find_elements_by_tag_name('option'))
 
                     for index_crops in range(0, num_options_crops):
-                        # Make the unique combination of options from the 8 dropdowns
+                        # Make the unique combination of options from the 6 dropdowns
                         dropdown_year = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlYear")
                         dropdown_year.find_elements_by_tag_name("option")[index_year].click()
 
                         dropdown_social_group = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlSocialGroup")
                         # Click only the "ALL SOCIAL GROUPS" option
                         [x for x in dropdown_social_group.find_elements_by_tag_name("option") if
-                         x.get_attribute('value') == 'ALL SOCIAL GROUPS'][index_social_group].click()
+                         x.get_attribute('value') == '4'][index_social_group].click()
 
                         dropdown_state = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlState")
                         dropdown_state.find_elements_by_tag_name("option")[index_state].click()
@@ -50,7 +53,7 @@ for index_year in range(0, num_options_year):
 
                         dropdown_tables = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlTables")
                         [x for x in dropdown_tables.find_elements_by_tag_name("option")
-                            if x.get_attribute('value') == 'TENANCY' or x.get_attribute('value') == 'CROPPING PATTERN'][index_tables].click()
+                            if x.get_attribute('value') == '6b'][index_tables].click()
 
                         dropdown_crops = driver.find_element_by_id("_ctl0_ContentPlaceHolder1_ddlCrop")
                         dropdown_crops.find_elements_by_tag_name("option")[index_crops].click()
