521fc2ea914e59eb693af03774d7db84ebd90016
Soohyun Kim <shkim7534@gmail.com>
2018-01-26 14:04:58 -0800

    convert xlsx to csv

---
+++ selenium_scraper/data_dump_xlsx/xlsxToCsv.js
@@ -0,0 +1,30 @@
+let fs = require("fs");
+let xlsx = require("node-xlsx");
+let dir = fs.readdirSync(__dirname);
+
+// filter out .xlsx files
+dir = dir.filter(function (a) {
+  return a.includes('.xlsx');
+});
+
+
+dir.forEach(function(file) {
+    let xlsxObj = xlsx.parse(__dirname + '/' + file);
+    let rows = [];
+    xlsxObj.map(function (sheet) {
+        //loop through all rows in the sheet and extract it
+        sheet.data.map(function(j) {
+          rows.push(j);
+        });
+        let writeStr = '';
+        rows.map(function (s) {
+          writeStr += s.join(',') + '\n';
+        });
+        fs.writeFile(__dirname + '/' + file.split('.')[0] + '.csv', writeStr, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log(file + " was saved in the current directory!");
+        });
+    })
+  });
