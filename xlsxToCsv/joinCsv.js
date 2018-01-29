3b572245662f0f3183fc272c09244be35d2cf77a
Soohyun Kim <shkim7534@gmail.com>
2018-01-29 14:48:46 -0800

    using csv-parse

--- xlsxToCsv/joinCsv.js
+++ xlsxToCsv/joinCsv.js
@@ -1,3 +1,16 @@
 let fs = require("fs");
-let xlsx = require("node-xlsx");
 let dir = fs.readdirSync(__dirname + '/dataCsv/');
+let csv = require('csv');
+var parse = require('csv-parse');
+require('should');
+dir = dir.filter(function (a) {
+    return a.includes('.csv');
+});
+
+dir.forEach(function (inputPath) {
+    fs.readFile(__dirname + '/dataCsv/' + inputPath, function (err, fileData) {
+        parse(fileData, {from: 3, columns: true, relax_column_count: true}, function (data) {
+            console.log(data)
+        });
+    })
+});
