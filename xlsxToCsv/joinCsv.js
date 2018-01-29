890fa73164429d122ca6fce76229d5984a703b8b
Justin Lee <justinlee0022@gmail.com>
2018-01-29 15:47:51 -0800

    Fixed bug with callback in joincsv

--- xlsxToCsv/joinCsv.js
+++ xlsxToCsv/joinCsv.js
@@ -1,16 +1,16 @@
-let fs = require("fs");
-let dir = fs.readdirSync(__dirname + '/dataCsv/');
-let csv = require('csv');
-var parse = require('csv-parse');
-require('should');
-dir = dir.filter(function (a) {
-    return a.includes('.csv');
-});
+const fs = require('fs')
+let dir = fs.readdirSync(__dirname + '/dataCsv/')
+const parse = require('csv-parse')
+const assert = require('assert')
 
-dir.forEach(function (inputPath) {
-    fs.readFile(__dirname + '/dataCsv/' + inputPath, function (err, fileData) {
-        parse(fileData, {from: 3, columns: true, relax_column_count: true}, function (data) {
-            console.log(data)
-        });
+dir = dir.filter(file => file.includes('.csv'))
+
+dir.forEach(inputPath => {
+  fs.readFile(__dirname + '/dataCsv/' + inputPath, (err, fileData) => {
+    assert.equal(null, err)
+    parse(fileData, {from: 3, columns: true, relax_column_count: true}, (err, data) => {
+      assert.equal(null, err)
+      console.log(data)
     })
-});
+  })
+})
