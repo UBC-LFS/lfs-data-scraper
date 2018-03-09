4aec9d7018afda5bdb130b3c14c9097633fe51f5
Soohyun Kim <shkim7534@gmail.com>
2018-03-09 13:40:20 -0800

    use streams instead of append

--- xlsxToCsv/joinCsv.js
+++ xlsxToCsv/joinCsv.js
@@ -1,24 +1,19 @@
 const fs = require('fs');
 let dir = fs.readdirSync(__dirname + '/csv/');
-const { promisify } = require('util')
+const { promisify } =  require('util')
 const parse = require('csv-parse');
 const assert = require('assert');
 
-const fsWriteFile = promisify(fs.writeFile)
-const fsReadFile = promisify(fs.readFile)
-const fsAppendFile = promisify(fs.appendFile)
-
-const outputName = 'output.csv'
+const fsWriteFile = promisify(fs.writeFile);
+const fsReadFile = promisify(fs.readFile);
+const outputName = 'output.csv';
+const stream = fs.createWriteStream(__dirname + '/output/' + outputName, {flags:'a'});
 
 dir = dir.filter(file => file.includes('.csv'));
 
-const appendCSV = async obj => {
-    await fsAppendFile(__dirname + '/output/' + outputName, obj + '\r\n')
-}
-
 const writeHeader = async () => {
     const header = ['Year', 'Crop', 'State', 'District', 'Class / SI Name', 'Number of Holdings', 'Irrigated Area', 'Unirrigated Area', 'Total Area'];
-    await fsWriteFile(__dirname + '/output/' + outputName, header + '\r\n')
+    await fsWriteFile(__dirname + '/output/' + outputName, header + '\r\n');
 }
 
 const readCSVs = async dir => {
@@ -37,11 +32,11 @@ const readCSVs = async dir => {
               const irrigated = data[i][getFilteredColumn(data[6],'Irrigated')];
               const unirrigated = data[i][getFilteredColumn(data[6],'Unirrigated')];
               const total = data[i][getFilteredColumn(data[6],'Total')];
-              appendCSV([year, crop, state, district, sl, holdings, irrigated, unirrigated, total]);
-            }
+              stream.write([year, crop, state, district, sl, holdings, irrigated, unirrigated, total] + '\r\n');
+            };
         });
-    })
-}
+    });
+};
 
 readCSVs(dir);
 
