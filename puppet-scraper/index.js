3e1cdb3f956dceaa970a33ff6b1477a5bf723496
Justin Lee <justinlee0022@gmail.com>
2018-03-13 15:27:17 -0700

    Working on puppeteer implementation to see if headless chrome can scrape faster

---
+++ puppet-scraper/index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false
+  })
+  const page1 = await browser.newPage()
+  page1.on('console', msg => console.log('PAGE LOG:', msg.text()))
+  await page1.goto('http://agcensus.dacnet.nic.in/DistCharacteristic.aspx')
+
+  // const page2 = await browser.newPage()
+  // await page2.goto('http://agcensus.dacnet.nic.in/DistCharacteristic.aspx')
+
+  const yearSelector = '_ctl0_ContentPlaceHolder1_ddlYear'
+  const socialGroupSelector = '_ctl0_ContentPlaceHolder1_ddlSocialGroup'
+  const stateSelector = '_ctl0_ContentPlaceHolder1_ddlState'
+  const districtSelector = '_ctl0_ContentPlaceHolder1_ddlDistrict'
+  const tablesSelector = '_ctl0_ContentPlaceHolder1_ddlTables'
+  const cropSelector = '_ctl0_ContentPlaceHolder1_ddlCrop'
+
+  // const year = await page1.select('#_ctl0_ContentPlaceHolder1_ddlYear')
+  await page1.evaluate(() => {
+    const yearElement = document.getElementById('_ctl0_ContentPlaceHolder1_ddlYear').children
+    console.log(yearElement)
+    return yearElement
+  })
+})()
