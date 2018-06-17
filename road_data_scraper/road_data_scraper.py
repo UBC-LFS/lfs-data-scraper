e2f7a66f791fb4e637434e24048612f4d1982116
Jason Doo <jeong7496@gmail.com>
2018-06-16 17:39:12 -0700

    Fix chromedriver path

--- road_data_scraper/road_data_scraper.py
+++ road_data_scraper/road_data_scraper.py
@@ -6,21 +6,20 @@ from selenium.webdriver.support.ui import Select
 import time
 import csv
 
-driver = webdriver.Chrome('C:/Users/Jeong/Downloads/chromedriver_win32/chromedriver.exe') # PATH TO DRIVER
-driver.implicitly_wait(5) # seconds
-
+driver = webdriver.Chrome()
+driver.implicitly_wait(5) 
 driver.get('http://omms.nic.in/')
 
 completed_road_works_button = driver.find_element_by_css_selector("div.small-box.bg-red > a")
 completed_road_works_button.click()
 
-# select a state
+# Select a state
 state_select = Select(driver.find_element_by_css_selector("select[title='State']"))
 state_options = state_select.options
 for i in range(1, len(state_options)):
   state_select.select_by_index(i)
 
-  # for each state, select a district
+  # For each state, select a district
   district_select = Select(driver.find_element_by_css_selector("select#ddlSPDistrict"))
   district_options = district_select.options
 
